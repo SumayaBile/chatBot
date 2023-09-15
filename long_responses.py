dc18b351f0ac445ede930c3d5cd67b66b1c0a6ff
Sumaya bile <92338759+SumayaBile@users.noreply.github.com>
2023-09-15 23:05:28 +0300

    Add files via upload

---
+++ long_responses.py
@@ -0,0 +1,18 @@
+import random
+
+R_EATING = "I don't like eating anything because I'm a bot obviously!"
+R_ADVICE = "If I were you, I would go to the internet and type exactly what you wrote there!"
+R_feeling = "I feel great!"
+R_feeling = "I don't have age , I'm virtual friend here to chat with you !!"
+
+
+
+
+
+def unknown():
+    response = ["Could you please re-phrase that? ",
+                "...",
+                "Sounds about right.",
+                "What does that mean?"][
+        random.randrange(4)]
+    return response
